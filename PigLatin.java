bd832793d80272959215138290b0ba17a7b857fe
ESGit3 <ethanshan706@gmail.com>
2021-01-05 20:07:30 -0500

    fixed piglatinsimple

--- PigLatin.java
+++ PigLatin.java
@@ -1,9 +1,18 @@
 public class PigLatin {
     public static String pigLatinSimple(String s) {
         char[] vowels = {'a', 'e', 'i', 'o', 'u'};
-        if (vowels.contains(s.charAt(0))) {
-            return s + "hay";
+        for (char i : vowels) {
+            if (i == s.charAt(0)) {
+                return s + "hay";
+            }
         }
         return s.substring(1) + s.substring(0, 1) + "ay";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(pigLatinSimple("mock"));
+        System.out.println(pigLatinSimple("pie"));
+        System.out.println(pigLatinSimple("david"));
+        System.out.println(pigLatinSimple("aaron"));
     }
 }
